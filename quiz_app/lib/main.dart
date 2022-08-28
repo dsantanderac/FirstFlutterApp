8291e3107119738a1ad6305abc974c421edb27c7
Daniel Santander <santanderad00@outlook.com>
2022-08-27 23:55:17 -0400

    Avance usando widgets

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
